00ee2bcddb186d51994e7969294d5d99af31a459
Christopher Osborn <z68ozz@gmail.com>
2020-05-09 23:34:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,40 @@
-# Make your shoe class here!
+class Shoe 
+  
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  def brand 
+    @brand 
+  end
+  
+  def color=(color)
+    @color = color 
+  end 
+  
+  def color 
+    @color 
+  end 
+  
+  def size=(size)
+    @size = size
+  end 
+  
+  def material=(material)
+    @material = material 
+  end
+  
+  def material
+    @material
+  end 
+  
+  def condtion=(condtion)
+    @condtion = condtion
+  end 
+  
+  def condtion
+    @condtion
+  end 
+  
+  def cobble
+    puts 
